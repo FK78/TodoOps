13bdafa31c9ef818dbce66b7e34bbdf256d7241a
FK78 <34196381+FK78@users.noreply.github.com>
2026-07-28 14:52:34 +0100

    chore(infra): fix Terraform syntax and resource references in main.tf

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -60,7 +60,7 @@ data "aws_ami" "ubuntu" {
 
   filter {
      name = "name"
-     values = "ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-arm64-server-*"
+     values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-arm64-server-*"]
   }
 
   filter {
@@ -70,7 +70,7 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "todo_ops" {
-    ami = data.aws_ami.ubuntu
+    ami = data.aws_ami.ubuntu.id
     instance_type = var.instance_type
     key_name = aws_key_pair.deployer.key_name
     vpc_security_group_ids = [aws_security_group.todo_ops.id]
