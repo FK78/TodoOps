43b6c18e85c7f751cd50e1112e657cf775d7a049
FK78 <34196381+FK78@users.noreply.github.com>
2026-07-28 15:28:13 +0100

    chore(infra): add Terraform configuration for Hetzner Cloud deployment

---
+++ infra/terraform-hetzner/main.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_providers {
+    hcloud = {
+        source = "hetznercloud/hcloud"
+        version = "1.67.0"
+    }
+    tls = {
+        source = "hashicorp/tls"
+        version = "4.3.0"
+    }
+    local = {
+        source = "hashicorp/local"
+        version = "2.9.0"
+    }
+  }
+}
+
+provider "hcloud" {
+  token = var.hcloud_token
+}
+
+resource "hcloud_ssh_key" "deployer" {
+  name = var.key_name
+  public_key = tls_private_key.ssh.public_key_openssh
+}
+
+resource "local_file" "ssh_private_key" {
+  content = "tls_private_key.ssh.private_key_openssh"
+  filename = "${path.module}.${var.key_name}.pem"
+  file_permission = "0600"
+}
+
+resource "hcloud_firewall" "todo_ops" {
+  name = "todo-ops-fw"
+
+  rule {
+    direction = "in"
+    protocol = "tcp"
+    port = "22"
+    source_ips = ["0.0.0.0/0", "::/0"]
+  }
+
+  rule {
+    direction = "in"
+    protocol = "tcp"
+    port = "3000"
+    source_ips = ["0.0.0.0/0", "::/0"]
+  }
+}
+
+resource "hcloud_server" "todo_ops" {
+  name = "todo-ops-server"
+  image = "ubuntu-24.04"
+  server_type = "cax11"
+  location = var.location
+  ssh_keys = [hcloud_ssh_key.deployer.id]
+  firewall_ids = [hcloud_firewall.todo_ops.id]
+}
